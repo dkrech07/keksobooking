424cdb520253b602de58420348f81cfc9d9cce55
dkrech07 <dkrech07@gmail.com>
2019-03-27 21:43:54 +0500

    наполняю массив объектов

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-getRandom // СПИСОК КОНСТАНТ
+// СПИСОК КОНСТАНТ
 var ADS_NUBMER = 8; // Количество объявлений в сформированном массиве;
 var LOCATION_X = 600;
 var LOCATION_Y = 1000;
@@ -7,8 +7,6 @@ var MAX_PRICE = 1000000;
 var ROOMS_NUMBER = 5;
 var GUESTS_NUMBER = 10;
 
-// СПИСОК ПЕРЕМЕННЫХ
-
 // СПИСОК МАССИВОВ И ОБЪЕКТОВ
 var DESCRIPTION_APARTAMENT = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый гостевой домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
 var TYPE_HOUSING = ["palace", "flat", "house", "bungalo"];
@@ -16,18 +14,15 @@ var CHECK_IN = ["12:00", "13:00", "14:00"];
 var CHECK_OUT = ["12:00", "13:00", "14:00"];
 var FEATURES_LIST = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
+// СПИСОК ПЕРЕМЕННЫХ
 var randomPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-
 var featureList = []; // Генерирую массив случайных особенностей;
-for (var i = 0; i < getRandom(FEATURES_LIST.length); i++) {
-  featureList[i] = (FEATURES_LIST[getRandom(FEATURES_LIST.length)]);
-}
-
-
-console.log("Массив удобств: " + featureList);
-
 var adsArray = []; // Массив объявлений. В него будут записаны сгенерированные объекты;
 var similarAds = {}; // Обявление. Оъект в который будут записаны данные после выполнения цикла for;
+
+for (var i = 0; i < getRandom(FEATURES_LIST.length) + 1; i++) { // Цикл для записи случайных элементов в массив;
+  featureList[i] = (FEATURES_LIST[getRandom(FEATURES_LIST.length)]);
+}
 
 // СПИСОК ФУНКЦИЙ
 function getRandom(n) { // Генерация случайного числа с округлением, полученное значение всегда меньше n;
@@ -37,6 +32,19 @@ function getRandom(n) { // Генерация случайного числа с
 
 function getRandomDouble(min, max) { // Генерация случайного числа между двумя значениями;
   return Math.floor(Math.random() * (max - min) + min);
+}
+
+function featureListUnique(arr) { // Оставить уникальные элементы массива;
+  var result = [];
+  nextInput:
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i]; // для каждого элемента
+      for (var j = 0; j < result.length; j++) { // ищем, был ли он уже?
+        if (result[j] == str) continue nextInput; // если да, то следующий
+      }
+      result.push(str);
+    }
+  return result;
 }
 
 function createAd(n) { // Функция генерации предложения;
@@ -55,7 +63,7 @@ function createAd(n) { // Функция генерации предложени
         guests: getRandom(GUESTS_NUMBER) + 1, // Количество гостей;
         checkin: CHECK_IN[getRandom(CHECK_IN.length)], // Случайное время заезда, формируется из массива CHECK_IN;
         checkout: CHECK_OUT[getRandom(CHECK_OUT.length)], // Случайное время выезда, формируется из массива CHECK_OUT;
-        features: featureList //Получаю из массива случайных особенностей строку;
+        features: featureListUnique(featureList) //Получаю из массива случайных особенностей строку;
       }
     };
     adsArray[i] = similarAds; // Кладу созданный объект в массив;
@@ -65,13 +73,8 @@ function createAd(n) { // Функция генерации предложени
 
 var adsAll = createAd(ADS_NUBMER); // Созданный в функции массив объявлений с объектами;
 
-console.log("Список преимуществ: " + FEATURES_LIST[getRandom(FEATURES_LIST.length)]);
-console.log(getRandom(LOCATION_X));
-console.log(getRandom(LOCATION_Y));
-console.log(adsArray);
-console.log(adsArray[1]);
-console.log('Мин и Макс цена: ' + getRandomDouble(MIN_PRICE, MAX_PRICE));
-console.log(getRandom(GUESTS_NUMBER) + 1);
-console.log('Время заезда: ' + CHECK_IN[getRandom(CHECK_IN.length)]);
-console.log('Время выезда: ' + CHECK_OUT[getRandom(CHECK_OUT.length)]);
+
+console.log("Массив удобств: " + featureList);
+console.log("Обработанный массив: " + featureArr);
+
 // console.log('Средняя цена: ' + randomPrice);
